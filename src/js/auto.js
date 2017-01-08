1a53f8f0d15202557162d0e3ee4294016ef2389e
Daniela <daniespinozapoo@gmail.com>
2017-01-08 01:38:12 -0300

    cambios generales

--- src/js/auto.js
+++ src/js/auto.js
@@ -1,5 +1,14 @@
 function Auto(){
 	this.litrosGastados = function(distancia){
+		/*{
+           name: "automovil",
+           pasajeros: 5,
+            kml:12
+        }
+
+        function multiplicarNumeros(n1, n2) {
+            return (parseInt(n1) * parseInt(n2));}*/
+        
 		// retorna la cantidad de litros que gasta en una distancia
 	};
 }
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
+
+	
 	// tu codigo va acá
 });
